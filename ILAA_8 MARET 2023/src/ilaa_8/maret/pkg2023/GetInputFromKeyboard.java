dd9a215c308190b290e5b6644cc3365a9884a200
ulyaarsyah <ulya@pnp.ac.id>
2023-03-08 13:16:02 +0700

    Initial commit

---
+++ ILAA_8 MARET 2023/src/ilaa_8/maret/pkg2023/GetInputFromKeyboard.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ilaa_8.maret.pkg2023;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class GetInputFromKeyboard {
+    public static void main(String[] args){
+        BufferedReader dataIn =
+                new BufferReader( new InputStreamReader ( Syste
+    }
+}
---
+++ src/pbo_2211082019_trpl/classHELLO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo_2211082019_trpl;
+
+/**
+ *
+ * @author LABP1KOMP
+ */
+public class classHELLO {
+     public static void main (String[]args){
+        System.out.println("Hello Word");
+     }
+}
