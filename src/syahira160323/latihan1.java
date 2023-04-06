290a9acc0f6ee52d8d23d72b473c3522a7947518
SitiNurSyahira2 <127277531+SitiNurSyahira2@users.noreply.github.com>
2023-04-06 10:14:50 +0700

    push

--- src/syahira160323/latihan1.java
+++ src/syahira160323/latihan1.java
@@ -3,34 +3,40 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package syahira160323;
+package adit160323;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author LABP1KOMP
  */
-public class latihan1 {
-    public static void main(String[] args){
-        BufferedReader dataIn = new BufferedReader(new InputStreamReader( System.in) );
+public class Latihan1 {
+    public static void main(String []args) throws IOException{
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader( System.in ) );
         int nilai1;
         int nilai2;
         boolean d;
-        try {
-            System.out.print("Entri nilai A : ");
+        
+        
+        
+        try{
+            System.out.print("Entri nilai A :");
             nilai1 = Integer.parseInt(dataIn.readLine());
-            System.out.print("Entri nilai B : ");
+            System.out.print("Entri nilai B :");
             nilai2 = Integer.parseInt(dataIn.readLine());
-            System.out.print("Entri nilai D : ");
+            System.out.print("Entri nilai d :");
             d = Boolean.parseBoolean(dataIn.readLine());
-            
+
             int nilai3 = nilai1 + nilai2;
-            System.out.println("hasil nilai C : " + nilai3);
-            System.out.println("Hasil nilai D : " + d);
-        }catch(IOException e){
-            
+            System.out.println("Entri nilai C :" + nilai3);
+            System.out.println("Entri nilai D :" + d);
+            } catch (Exception ex){
+            }
         }
+                
+        
     }
     
-}
+
